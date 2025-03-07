c795939afb57450a50dff8d5f7a24c439b1c1b0b
Kiwrght <kiwrght@uiowa.edu>
2025-03-07 13:08:48 -0600

    Updated Fetch
    Updated fetch method to not use the promise method.

--- script.js
+++ script.js
@@ -16,23 +16,31 @@ table.style.backgroundColor = "white";
 
 /* Adding Data to the Table*/
 const URL = "https://datausa.io/api/data?drilldowns=Nation&measures=Population" 
-fetch(URL)
-.then(response => response.json())// makes a promise to get the data
-.then(data => {
-    console.log(data)
-    var tdata = []
-    for(var i = 0; i < data.data.length; i++){
-        tdata.push([data.data[i].Year, data.data[i].Population])
+
+async function fetchData(params) {
+    const response = await fetch(URL);
+    const data = await response.json();
+    console.log(data);
+
+    const tdata = [];
+
+    for (let i = 0; i < data.data.length; i++) {
+        tdata.push(data.data[i].Nation,
+            data.data[i].Population); 
     }
-    console.log(tdata)
-    //add data to the tbody of the table 
-    for(var i = 0; i < tdata.length; i++){
-        var row = table.insertRow();
-        for(var j = 0; j < tdata[i].length; j++){
-            var cell = row.insertCell(j);
+
+    console.log(tdata);
+
+    for (let i = 0; i < tdata.length; i++) {
+        let row = tbody.insertRow();
+        for (let j = 0; j < tdata[i].length; j++) {
+            let cell = row.insertCell(j);
             cell.innerHTML = tdata[i][j];
         }
+        
     }
-})
+}
+
+fetchData();
 
 console.log("Hello World");
